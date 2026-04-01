1275d1b1ddfae23f241c22c2d964aa775a47f63f
MaydelGamerZ <TU_CORREO_DE_GITHUB>
2026-03-31 22:31:41 -0600

    Actualiza diseño con modo oscuro y nuevas secciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
+  // Habilitamos dark mode usando la clase `dark` en el elemento raíz.
+  darkMode: 'class',
   theme: {
     extend: {},
   },
